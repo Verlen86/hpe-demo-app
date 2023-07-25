c990189ebcdc80b54792df8fa6099f839fcbcd01
Verlen86 <verlen86@gmail.com>
2023-07-26 00:19:58 +0300

    Update BandTest.java

--- webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
+++ webapp/src/test/java/com/hp/devops/demoapp/BandTest.java
@@ -7,7 +7,7 @@ import org.junit.Ignore;
 import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
-import java.math.BigInteger
+import java.math.BigInteger;
 
 import java.security.InvalidParameterException;
 
